4f3311ebcb57b1570fe4dc5f90a8b29dad7e1e95
Axel Roszkopf <axelsky@gmail.com>
2018-07-18 00:48:20 +0200

    set production mode + db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ideapool',
-    'username' => 'root',
-    'password' => 'root',
+//    'dsn' => 'mysql:host=localhost;dbname=ideapool',
+    'dsn' => 'pgsql:host=ec2-23-21-238-28.compute-1.amazonaws.com;port=5432;dbname=d893o1rd59nnbj',
+    'username' => 'ypmhsfbyzdfzxj',
+    'password' => '86a190e6e5c6653fbc41413708a36df5906b0d18a82917b6478c34b81c1252ce',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
